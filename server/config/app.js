44cebcd4268a43d6e9c77f5c0be4c919fd1a23c2
andrewmon <musicstar@bellsouth.net>
2017-09-29 13:47:22 -0400

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -3,6 +3,7 @@ var config = require('./config'),
     express = require('./express');
 
 module.exports.start = function() {
+  var port = (process.env.PORT || 8080);
   var app = express.init();
   app.listen(config.port, function() {
     console.log('App listening on port', config.port);
